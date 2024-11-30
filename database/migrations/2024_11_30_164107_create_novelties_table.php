16b3799b48f4a29df0e5c9aa9828bccbccf40c26
Camilo <camilocamacho1006@hotmail.com>
2024-11-30 15:11:48 -0500

    🚧 Se agrega el recurso de novedades

---
+++ database/migrations/2024_11_30_164107_create_novelties_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('novelties', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('owner_id')->constrained('users')->cascadeOnDelete();
+            $table->string('title');
+            $table->text('description');
+            $table->string('filepath')->nullable();
+            $table->timestamp('published_at')->nullable();
+            $table->boolean('active')->default(false);
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('novelties');
+    }
+};
