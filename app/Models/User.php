40a6d9173d13e45a82d971ec1e9c0c334a4cd124
Camilo <camilocamacho1006@hotmail.com>
2024-12-02 08:07:45 -0500

    🚧 Se agrega shield

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,20 +3,37 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+use App\Filament\Erp\Resources\UserResource\Pages\CreateUser;
+use App\Filament\Erp\Resources\UserResource\Pages\EditUser;
+use BezhanSalleh\FilamentShield\Traits\HasPanelShield;
+use Filament\Forms\Components\Grid;
+use Filament\Forms\Components\Section;
+use Filament\Forms\Components\Select;
+use Filament\Forms\Components\TextInput;
+use Filament\Models\Contracts\FilamentUser;
+use Filament\Panel;
+use Filament\Tables\Columns\TextColumn;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Spatie\Permission\Traits\HasRoles;
 
-class User extends Authenticatable
+class User extends Authenticatable implements FilamentUser
 {
-    /** @use HasFactory<\Database\Factories\UserFactory> */
-    use HasFactory, Notifiable;
+    use HasFactory, HasPanelShield, HasRoles, Notifiable;
+    use SoftDeletes;
 
     /**
      * The attributes that are mass assignable.
      *
      * @var array<int, string>
      */
+    public function canAccessPanel(Panel $panel): bool
+    {
+        return true;
+    }
+
     protected $fillable = [
         'name',
         'email',
@@ -43,6 +60,96 @@ class User extends Authenticatable
         return [
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
+        ];
+    }
+
+    protected $dates = ['deleted_at'];
+
+    public static $NAME = 'Nombre';
+
+    public static $EMAIL = 'Correo electrónico';
+
+    public static $PASSWORD = 'Contraseña';
+
+    public static $PASSWORD_CONFIRMATION = 'Confirmar contraseña';
+
+    public static $ROLES = 'Roles';
+
+    public static function getForm(): array
+    {
+        return [
+            Section::make('')
+                ->schema([
+                    TextInput::make('name')
+                        ->label(self::$NAME)
+                        ->required()
+                        ->maxLength(255),
+                    TextInput::make('email')
+                        ->label(self::$EMAIL)
+                        ->unique(ignoreRecord: true)
+                        ->email()
+                        ->required(),
+
+                    Select::make('roles')
+                        ->label(self::$ROLES)
+                        ->preload()
+                        ->relationship('roles', 'name')
+                        ->multiple()
+                        ->required(),
+                    Grid::make(2)
+                        ->visibleOn(['create'])
+                        ->schema([
+                            TextInput::make('password')
+                                ->label(self::$PASSWORD)
+                                ->revealable()
+                                ->confirmed()
+                                ->password()
+                                ->required(),
+                            TextInput::make('password_confirmation')
+                                ->label(self::$PASSWORD_CONFIRMATION)
+                                ->revealable()
+                                ->password()
+                                ->live(onBlur: false)
+                                ->afterStateUpdated(function (CreateUser|EditUser $livewire): void {
+                                    $livewire->validateOnly('data.password');
+                                })
+                                ->required(),
+                        ]),
+
+                    Grid::make(2)
+                        ->visibleOn(['edit'])
+                        ->schema([
+                            TextInput::make('password')
+                                ->label(self::$PASSWORD)
+                                ->revealable()
+                                ->confirmed()
+                                ->password(),
+                            TextInput::make('password_confirmation')
+                                ->label(self::$PASSWORD_CONFIRMATION)
+                                ->revealable()
+                                ->password()
+                                ->live(onBlur: false)
+                                ->afterStateUpdated(function (CreateUser|EditUser $livewire): void {
+                                    $livewire->validateOnly('data.password');
+                                }),
+                        ]),
+                ]),
+
+        ];
+    }
+
+    public static function getColumns(): array
+    {
+        return [
+            TextColumn::make('name')
+                ->label(self::$NAME)
+                ->searchable(),
+            TextColumn::make('email')
+                ->label(self::$EMAIL)
+                ->searchable(),
+            TextColumn::make('roles.name')
+                ->label(self::$ROLES)
+                ->badge(),
         ];
     }
 }
--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -42,8 +42,8 @@ class AdminPanelProvider extends PanelProvider
             ->discoverWidgets(in: app_path('Filament/Widgets'), for: 'App\\Filament\\Widgets')
             ->widgets([
                 Widgets\AccountWidget::class,
-                Widgets\FilamentInfoWidget::class,
             ])
+            ->plugins([\BezhanSalleh\FilamentShield\FilamentShieldPlugin::make()])
             ->middleware([
                 EncryptCookies::class,
                 AddQueuedCookiesToResponse::class,
